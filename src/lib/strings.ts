6927405eab66200e9d8b55fd8c96d65d6d3a1a13
daniel <dvde@gmx.net>
2023-02-01 19:28:37 +0100

    add FirstChar, WithoutFirstChar

--- src/lib/strings.ts
+++ src/lib/strings.ts
@@ -18,17 +18,24 @@ export type UppercaseF = typeof UppercaseF;
 export const LowercaseF: unique symbol = Symbol();
 export type LowercaseF = typeof LowercaseF;
 
+export const FirstCharF: unique symbol = Symbol();
+export type FirstCharF = typeof FirstCharF;
+
+export const WithoutFirstCharF: unique symbol = Symbol();
+export type WithoutFirstCharF = typeof WithoutFirstCharF;
+
 declare module '../hkt' {
     interface Kind1Table<T> {
         [SplitStringF]: T extends string ? SplitString<T> : never
         [JoinStringsF]: T extends string[] ? JoinStrings<T> : never
         [UppercaseF]: T extends string ? Uppercase<T> : never
         [LowercaseF]: T extends string ? Lowercase<T> : never
+        [FirstCharF]: T extends `${infer C}${string}` ? C : never
+        [WithoutFirstCharF]: T extends `${string}${infer R}` ? R : never
     }
     interface Kind2Table<T1, T2> {
         [AppendStringsF]: T1 extends string ? T2 extends string ? AppendStrings<T1, T2> : never : never;
         [StartsWithF]: T1 extends string ? T2 extends string ? StartsWith<T1, T2> : never : never;
-
     }
 }
 
